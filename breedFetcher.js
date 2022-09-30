cec9e9b3d25f0893bda6dd957a0a6d9cc82eee98
Anit <anittankandasamy@gmail.com>
2022-09-30 03:53:31 +0000

    finding and testing breed search api

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,1 +1,24 @@
 const request = require('request');
+
+const breedName = process.argv[2];
+
+const grabBreedDescription = (breedName, callback) => {
+  request((`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`), (err, respo, body) => {
+    //console.log(body);
+    //console.log(typeof body);
+  
+    if (err) {
+      return console.log(err);
+    }
+
+    const data = JSON.parse(body);
+    //console.log(data);
+    if (data.length === 0) {
+      console.log(`${breedName} cannot be found`, null);
+    } else {
+      console.log(null,data[0].description);
+    }
+  })
+}
+
+console.log(grabBreedDescription(breedName));
